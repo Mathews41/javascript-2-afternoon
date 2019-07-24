c1188cea6261f9d88b134c200ed9ef7d26e3b5f3
Mark Mathews <marklmathews4@gmail.com>
2019-07-24 17:48:13 -0600

    finished afternoon project

--- practice.js
+++ practice.js
@@ -15,7 +15,12 @@
 */
 
 //Code here
+let me = {
+  name: 'mark',
+  age: 160,
 
+};
+console.log (me.name)
 ////////// PROBLEM 2 //////////
 
 /*
@@ -24,6 +29,14 @@
 */
 
 //Code here
+let favoriteThings = {
+  band: 'Juicewrld',
+  food: 'burritos',
+  person: 'Karen',
+  book: 'the da vinci code',
+  movie: 'interstellar',
+  holiday: 'Thanksgiving'
+};
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
@@ -31,14 +44,16 @@
 */
 
 //Code here
-
+favoriteThings.car = 'F1'
+favoriteThings.brand = 'Ford'
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
-
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 ////////// PROBLEM 3 //////////
 
 /*
@@ -50,13 +65,18 @@
 */
 
 //Code here
-
+let backPack =  {
+  
+};
+ let item = 'firstPocket'
+ backPack.color = 'blue'
+ backPack[item] = 'chapstick'
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+console.log(backPack)
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -64,6 +84,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
+
 
 ////////// PROBLEM 4 //////////
 
@@ -85,6 +106,8 @@ var user2 = {
 */
 
 //Code Here
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
